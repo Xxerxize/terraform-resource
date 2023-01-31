c8ba6ee2ff493fa10cd8c99ba5716584c8b7c778
Uyanwune Chekwube <79359078+Xxerxize@users.noreply.github.com>
2023-01-31 23:45:39 +0100

    Create first_ec2.tf
    this is step by step how to launch first ec2 instance on terraform

---
+++ first_ec2.tf
@@ -0,0 +1,25 @@
+<h1> Important note 
+
+Note that every region has a different AMI ID. the AMI IDs keeps changing, so you must ensure that you use the latest AMI ID from the AWS console.
+
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "PUT-YOUR-ACCESS-KEY-HERE"
+  secret_key = "PUT-YOUR-SECRET-KEY-HERE"
+}
+
+resource "aws_instance" "myec2" {
+   ami = "ami-082b5a644766e0e6f"
+   instance_type = "t2.micro"
+}
+
+
+
+
+Commands:
+
+terraform init to initiate terraform
+terraform plan to get nformation on what exactly terraform is about to create
+terraform apply to actually create the resource written in the configuration.
+  
+kindly note that is very important to run the Plan before the apply each time to see what will happen in advance.
